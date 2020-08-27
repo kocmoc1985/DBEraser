1fb015d610d3acf4a9f2dbc6a893a930808911c9
kocmoc1985 <kocmoc1985@gmail.com>
2020-08-27 16:13:23 +0200

    Implementing PHP-Upload

--- src/BuhInvoice/HelpBuh.java
+++ src/BuhInvoice/HelpBuh.java
@@ -5,11 +5,17 @@
  */
 package BuhInvoice;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.nio.charset.StandardCharsets;
@@ -36,11 +42,12 @@ public class HelpBuh {
 
     /**
      * [2020-08-27]
+     *
      * @param phpScriptName - example: "_http_buh"
      * @param phpFunctionName - example: "delete_entry"
      * @param json
      * @return
-     * @throws Exception 
+     * @throws Exception
      */
     public static String executePHPScript(String phpScriptName, String phpFunctionName, String json) throws Exception {
         String url = execute(phpScriptName, phpFunctionName, json);
@@ -153,6 +160,49 @@ public class HelpBuh {
 //        String temp = arr[1];
 //        System.out.println("HTTP REQ VAL: " + temp);
 //        return temp;
+    }
+
+    /**
+     * [2020-08-27]
+     *
+     * @throws MalformedURLException
+     * @throws ProtocolException
+     * @throws IOException
+     * @throws InterruptedException
+     */
+    public static void http_send_image() throws MalformedURLException, ProtocolException, IOException, InterruptedException {
+        //
+//        HttpURLConnection httpUrlConnection = (HttpURLConnection) new URL("http://www.mypage.org/upload.php").openConnection();
+        HttpURLConnection httpUrlConnection = (HttpURLConnection) new URL("http://www.mypage.org/_u_u_u_x_upload?filename=abc.def").openConnection();
+        httpUrlConnection.setDoOutput(true);
+        httpUrlConnection.setRequestMethod("POST");
+        OutputStream os = httpUrlConnection.getOutputStream();
+        //
+        Thread.sleep(1000);
+        BufferedInputStream fis = new BufferedInputStream(new FileInputStream("tmpfile.tmp"));
+        //
+        long totalByte = fis.available();
+        long byteTrasferred = 0;
+        //
+        for (int i = 0; i < totalByte; i++) {
+            os.write(fis.read());
+            byteTrasferred = i + 1;
+        }
+        //
+        os.close();
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(
+                        httpUrlConnection.getInputStream()));
+        //
+        String s = null;
+        //
+        while ((s = in.readLine()) != null) {
+            System.out.println(s);
+        }
+        //
+        in.close();
+        fis.close();
+        //
     }
 
     /**
