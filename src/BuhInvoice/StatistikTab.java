b834e8e07ef3c745369c42513a15daa455196d87
kocmoc1985 <kocmoc1985@gmail.com>
2021-05-17 16:02:59 +0200

    BuhInvoice: Many Essential changes to the "GRAFISK VY" not exactly in this commit but todays commits

--- src/BuhInvoice/StatistikTab.java
+++ src/BuhInvoice/StatistikTab.java
@@ -7,14 +7,17 @@ package BuhInvoice;
 
 import XYG_BARGRAPH.BARGraph;
 import XYG_BARGRAPH.MyGraphXY_BG;
+import XYG_BARGRAPH.MyPoint_BG;
 import XYG_BASIC.MyGraphContainer;
 import XYG_BASIC.MyPoint;
+import XYG_BASIC.MySerie;
 import XYG_STATS.BarGraphListener;
 import XYG_STATS.BasicGraphListener;
 import XYG_STATS.XyGraph_M;
 import XY_BUH_INVOICE.MyGraphXY_BuhInvoice;
 import XY_BUH_INVOICE.XyGraph_BuhInvoice;
 import forall.HelpA;
+import java.awt.Color;
 import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -28,6 +31,7 @@ import javax.swing.JPanel;
 import other.StringDouble;
 
 /**
+ * OBS! OBS! Showing Exkl Moms for both bargraph and common graphs
  *
  * @author HP-A
  */
@@ -36,6 +40,10 @@ public class StatistikTab implements BarGraphListener {
     private final BUH_INVOICE_MAIN bim;
     private ArrayList<HashMap<String, String>> fakturor_one_year_map;
     private final Object lock_a = new Object();
+    private XyGraph_BuhInvoice xygraph;
+
+    private final static String SERIE_NAME__BARGTAPH__TOTAL_PER_MONTH = "bar_graph_total_per_month";
+    private final static String SERIE_NAME__BARGTAPH__AMMOUNT_PER_MONTH = "bar_graph_ammount_per_month";
 
     public StatistikTab(BUH_INVOICE_MAIN bim) {
         this.bim = bim;
@@ -52,7 +60,7 @@ public class StatistikTab implements BarGraphListener {
         //
         drawGraph_basic(bim.jPanel_graph_panel_a, "all_invoices", DB.PHP_FUNC_PARAM_GET_KUND_FAKTUROR__ONE_YEAR_BACK);
         drawGraph_basic(bim.jPanel_graph_panel_b, "act_month", DB.PHP_FUNC_PARAM_GET_KUND_FAKTUROR__ACT_MONTH);
-        drawGraph_bargraph(bim.jPanel_graph_panel_c, bim.jPanel_graph_panel_d, "bar_graph_total_per_month", "bar_graph_ammount_per_month");
+        drawGraph_bargraph(bim.jPanel_graph_panel_c, bim.jPanel_graph_panel_d, SERIE_NAME__BARGTAPH__TOTAL_PER_MONTH, SERIE_NAME__BARGTAPH__AMMOUNT_PER_MONTH);
         //
     }
 
@@ -62,7 +70,7 @@ public class StatistikTab implements BarGraphListener {
         //
         drawGraph_basic(bim.jPanel_graph_panel_a, "all_invoices", DB.PHP_FUNC_PARAM_GET_KUND_FAKTUROR__ONE_YEAR_BACK);
         drawGraph_basic(bim.jPanel_graph_panel_b, "act_month", DB.PHP_FUNC_PARAM_GET_KUND_FAKTUROR__ACT_MONTH);
-        drawGraph_bargraph(bim.jPanel_graph_panel_c, bim.jPanel_graph_panel_d, "bar_graph_total_per_month", "bar_graph_ammount_per_month");
+        drawGraph_bargraph(bim.jPanel_graph_panel_c, bim.jPanel_graph_panel_d, SERIE_NAME__BARGTAPH__TOTAL_PER_MONTH, SERIE_NAME__BARGTAPH__AMMOUNT_PER_MONTH);
         //
     }
 
@@ -79,13 +87,17 @@ public class StatistikTab implements BarGraphListener {
         String dateNow = GP_BUH.getDate_yyyy_MM_dd();
         String dateFormat = GP_BUH.DATE_FORMAT_BASIC;
         //
-        XyGraph_BuhInvoice xghm = new XyGraph_BuhInvoice(name, new MyGraphXY_BuhInvoice(bim), MyGraphContainer.DISPLAY_MODE_FULL_SCREEN, dateNow, dateFormat);
+        XyGraph_BuhInvoice xygm = new XyGraph_BuhInvoice(name, DB.BUH_FAKTURA__TOTAL_EXKL_MOMS__, new MyGraphXY_BuhInvoice(bim), MyGraphContainer.DISPLAY_MODE_FULL_SCREEN, dateNow, dateFormat);
+        //
+        if (phpScript.equals(DB.PHP_FUNC_PARAM_GET_KUND_FAKTUROR__ONE_YEAR_BACK)) {
+            xygraph = xygm;
+        }
         //
         System.out.println("Thread:" + Thread.currentThread().getName());
         //
-        container.add(xghm.getGraph());
+        container.add(xygm.getGraph());
         //
-        Thread x = new Thread(new Thread_A_A(phpScript, xghm));
+        Thread x = new Thread(new Thread_A_A(phpScript, xygm));
         x.setName("Thread_A_A");
         x.start();
         //
@@ -102,7 +114,7 @@ public class StatistikTab implements BarGraphListener {
         //
         final XyGraph_M xygraph = new XyGraph_M(name_a, MyGraphContainer.DISPLAY_MODE_FULL_SCREEN);
         //
-        mgxyhm = new MyGraphXY_BG("Total",":-");
+        mgxyhm = new MyGraphXY_BG("Total", ":-");
         mgxyhm.addBarGraphListener(this);
         gg__total_per_month = new BARGraph(name_a, mgxyhm, MyGraphContainer.DISPLAY_MODE_FULL_SCREEN); // MyGraphContainer.DISPLAY_MODE_FOOT_DISABLED
         //
@@ -116,7 +128,7 @@ public class StatistikTab implements BarGraphListener {
         //
         final XyGraph_M xygraph_b = new XyGraph_M(name_b, MyGraphContainer.DISPLAY_MODE_FULL_SCREEN);
         //
-        mgxyhm_b = new MyGraphXY_BG("Antal"," st");
+        mgxyhm_b = new MyGraphXY_BG("Antal", " st");
         mgxyhm_b.addBarGraphListener(this);
         gg__ammount_per_month = new BARGraph(name_b, mgxyhm_b, MyGraphContainer.DISPLAY_MODE_FULL_SCREEN); // MyGraphContainer.DISPLAY_MODE_FOOT_DISABLED
         //
@@ -177,7 +189,7 @@ public class StatistikTab implements BarGraphListener {
             // OBS! HERE Below it's done with AWT-Thread
             java.awt.EventQueue.invokeLater(() -> {
                 System.out.println("Thread addData: " + Thread.currentThread());
-                this.xghm.addData(invoices, new String[]{"fakturadatum", "forfallodatum"});
+                this.xghm.addData(invoices, new String[]{DB.BUH_FAKTURA__FAKTURA_DATUM, DB.BUH_FAKTURA__FORFALLO_DATUM});
             });
             //
         }
@@ -218,9 +230,9 @@ public class StatistikTab implements BarGraphListener {
             //
             for (HashMap<String, String> map : fakturor_one_year_map) {
                 //
-                String fakturadatum = map.get("fakturadatum");
-                String total = map.get("total_ink_moms");
-                int fakturatyp = Integer.parseInt(map.get("fakturatyp"));
+                String fakturadatum = map.get(DB.BUH_FAKTURA__FAKTURA_DATUM);
+                String total = map.get(DB.BUH_FAKTURA__TOTAL_EXKL_MOMS__);
+                int fakturatyp = Integer.parseInt(map.get(DB.BUH_FAKTURA__FAKTURATYP));
                 //
                 String[] arr = fakturadatum.split("-");
                 String faktura_datum_short = arr[0] + "-" + arr[1];
@@ -245,7 +257,7 @@ public class StatistikTab implements BarGraphListener {
             while (it.hasNext()) {
                 String key = (String) it.next();
                 Double value = mont_sum_map.get(key);
-                System.out.println("key = " + key + "  value = " + value);
+//                System.out.println("key = " + key + "  value = " + value);
                 barGraphValuesList_total.add(new StringDouble(key, value));
             }
             //
@@ -302,13 +314,46 @@ public class StatistikTab implements BarGraphListener {
     }
 
     @Override
-    public void barGraphHoverEvent(MouseEvent me, MyPoint mp) {
-        // See MCLabStats 
+    public void barGraphHoverEvent(MouseEvent e, MyPoint mp) {
+        if (e.getSource() instanceof MyPoint_BG) {
+            //
+            MyPoint_BG mpbg = (MyPoint_BG) e.getSource();
+            //
+            if (mpbg.getSerieName().equals(SERIE_NAME__BARGTAPH__TOTAL_PER_MONTH)) {
+                highlight_a(mpbg, xygraph);
+            }
+            //
+        }
     }
 
     @Override
     public void barGraphHoverOutEvent(MouseEvent me) {
-        // See MCLabStats 
+        xygraph.getSerie().resetPointsColorAndForm();
+    }
+
+    private void highlight_a(MyPoint_BG mpbg, XyGraph_BuhInvoice xygraph_) {
+        //
+        MySerie serie = xygraph_.getSerie();
+        //
+        serie.resetPointsColorAndForm();
+        //
+        String monthYear = mpbg.getBarName(); // "2021-02"
+        //
+        for (MyPoint point : serie.getPoints()) {
+            //
+            HashMap<String, String> map = point.getPointInfo();
+            String fakturaDatum = map.get(DB.BUH_FAKTURA__FAKTURA_DATUM);
+            //
+            if (fakturaDatum.contains(monthYear)) {
+                point.setPointColor(Color.MAGENTA);
+                point.setPointDrawRect(true);
+            }
+            //
+        }
+        //
+        xygraph_.getGraph().revalidate();
+        xygraph_.getGraph().repaint();
+        //
     }
 
 }
