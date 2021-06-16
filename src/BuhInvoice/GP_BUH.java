75343ebb368ea23683413caef4a5d64fc1a23ce0
MCREMOTE <MCREMOTE@DESKTOP-HR23T6P>
2021-06-16 22:44:48 +0200

    BuhInvoice: implementing proportional rescaling

--- src/BuhInvoice/GP_BUH.java
+++ src/BuhInvoice/GP_BUH.java
@@ -441,9 +441,11 @@ public class GP_BUH {
             return new Dimension(0, 0);
         }
         //
-        int MAX_WIDTH = 170;
         int MIN_WIDTH = 120;
-        int MAX_HEIGHT = 90;
+        int MAX_WIDTH = 180;
+        int MAX_HEIGHT = 70;
+//        int MAX_WIDTH = 170;
+//        int MAX_HEIGHT = 90;
         //
         BufferedImage img;
         //
@@ -470,24 +472,48 @@ public class GP_BUH {
         int w_new = 0;
         int h_new = 0;
         //
-        if (w_orig > MAX_WIDTH) {
-            //
-            w_new = MAX_WIDTH;
-            //
-            if (h_orig > MAX_HEIGHT) {
-                h_new = MAX_HEIGHT; // This should be made more flexible***********[2020-09-04]
-            } else {
-                h_new = (int) (w_new / wh_proportion);
-            }
-            //
-
+        if (w_orig > MAX_WIDTH || h_orig > MAX_HEIGHT) {
+            return proportionalRescaling(wh_proportion, w_orig, h_orig, MAX_WIDTH, MAX_HEIGHT);
         } else {
-            w_new = w_orig;
-            h_new = h_orig;
+            return new Dimension(w_orig, h_orig);
         }
         //
-        return new Dimension(w_new, h_new);
+//        if (w_orig > MAX_WIDTH) {
+//            //
+//            w_new = MAX_WIDTH;
+//            //
+//            if (h_orig > MAX_HEIGHT) {
+//                h_new = MAX_HEIGHT; // This should be made more flexible***********[2020-09-04]
+//            } else {
+//                h_new = (int) (w_new / wh_proportion);
+//            }
+//            //
+//            //
+//        } else {
+//            w_new = w_orig;
+//            h_new = h_orig;
+//        }
+//        //
+//        return new Dimension(w_new, h_new);
         //
+    }
+
+    private static Dimension proportionalRescaling(double wh_proportion, int w_orig, int h_orig, int max_w, int max_h) {
+        //
+        double MAX_WIDTH = max_w;
+        double MAX_HEIGHT = max_h;
+        //
+        double w = w_orig;
+        double h = h_orig;
+        //
+        while (w > MAX_WIDTH || h > MAX_HEIGHT) {
+            w -= wh_proportion;
+            h -= 1;
+        }
+        //
+        System.out.println("");
+        //
+        return new Dimension((int)w, (int)h);
     }
 
     /**
